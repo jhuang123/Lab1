911a1e7211db233c47052889e2a21b446902cdc8
Vilarj <lamaravillasan@outlook.com>
2019-09-17 12:24:44 -0400

    Need to implement the last two methods
    - Intersection
    - Union

--- ResizableArrayBag.java
+++ ResizableArrayBag.java
@@ -1,4 +1,5 @@
 package lab1;
+import java.util.Arrays;
 
 /**
  * 
@@ -35,30 +36,91 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
 		return copybag.isEmpty();
 	}
 	// isEmpty method
-	private boolean isEmpty() {
-		if(bagarray == null) {assert(numberOfEntries == 0);return true;}
-		return false;
-		}
-	
-	//remove method
-	public boolean remove(T entry) {
-		
-		return false;}
-	
-	//toArray method
-	public T[] toArray() {
-		
-		return null;}
-	
+	public boolean isEmpty() {return (numberOfEntries == 0);}
+
 	// add method
-	public void add(String next) {}
-	//copy method
-	private ResizableArrayBag<T> copy() {
+	public boolean add(T entry) {
+		   // checkInitialization();
+		   if (isFull()){return false;}
+		   bagarray[numberOfEntries] = entry;
+		   numberOfEntries ++;
+		   
+		   return true;
+		}
+
+	// copy method
+	private ResizableArrayBag<T> copy() {return this;}
+	// full method
+	private boolean isFull () {return (numberOfEntries  == capacity);}
+
+	
+	@Override
+	public int getCurrentSize() {return numberOfEntries;}
+	@Override
+	public boolean remove(T entry){
+	    boolean found = false;
+	    
+	    for (int ii = 0; ii < numberOfEntries && !found; ii ++) {
+	        if (entry.equals(bagarray[ii]))
+				found = true;
+	        
+				bagarray[ii] =  bagarray[numberOfEntries - 1];
+				bagarray[numberOfEntries - 1] = null;
+				numberOfEntries--;
+	      }
+	    return found;
+	 }
+
+	@SuppressWarnings ("unchecked")
+	@Override
+	public void clear() {T[] clean = (T[]) new Object();bagarray = clean;}
+	@Override
+	public boolean contains(T anEntry) {
+		for(int i = 0; i < numberOfEntries; i++) {
+			if(bagarray[i] == anEntry) {return true;}
+		}
+		return false;
+	}
+	
+	@Override
+	public int getFrequencyOf(T anEntry) {
+		int frequency = 0;
+		
+		for(int i = 0; i < numberOfEntries; i++) {
+			if(bagarray[i] == anEntry) {frequency = frequency + i; return frequency;}
+		}
+		return 0;
+	}
+	
+	@Override
+	public T[] toArray () {
+	     @SuppressWarnings("unchecked")
+	     T [] result = (T[]) new Object[numberOfEntries];
+	     for (int count = 0; count < numberOfEntries; count ++) {
+	         result[count] = bagarray[count];
+	         }
+	     return result;
+	   }
+	
+	@Override
+	public T remove() {
+		if(isEmpty()) {return null;}
+		 T temp = bagarray[numberOfEntries - 1];
+		 numberOfEntries--;
+		 
+		return temp;
+	}
+	@Override
+	public T[] toArray(T[] a) {T[] array = Arrays.copyOf(bagarray, numberOfEntries);return array;}
+	
+	// intersection method
+	/*
+	 * Want to have the elements that appear in both bags
+	 */
+	public ResizableArrayBag<String> intersection(ResizableArrayBag<String> bag12) {
 		
 		return null;
 		}
-	// intersection method
-	public ResizableArrayBag<String> intersection(ResizableArrayBag<String> bag12) {return null;}
 	// union method
 	public ResizableArrayBag<String> union(ResizableArrayBag<String> bag22) {return null;}
 }
